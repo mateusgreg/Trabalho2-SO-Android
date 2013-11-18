8f9269073b6b2bec81b04bdae89f32acd01c8491
ramirezjpdf <ramirez.jpdf@ymail.com>
2013-11-17 23:51:43 -0200

    novas constantes o nome eh explicativo

--- Trabalho2-SO-Android/src/br/ufrj/dcc/util/Constantes.java
+++ Trabalho2-SO-Android/src/br/ufrj/dcc/util/Constantes.java
@@ -1,5 +1,8 @@
 package br.ufrj.dcc.util;
 
 public class Constantes {
-	public static int QTD_MAX_RECURSOS = 5000;
+	public static int QTD_MAX_RECURSOS = 100;
+	public static int N_PRODUTORES = 2;
+	public static int N_CONSUMIDORES = 3;
+	public static int CAPACIDADE_BUFFER = 20;
 }
