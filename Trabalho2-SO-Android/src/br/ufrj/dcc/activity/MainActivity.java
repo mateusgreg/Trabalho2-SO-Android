24b61f782ad4bf4dd403708758aa6f31d09e5af3
ramirezjpdf <ramirez.jpdf@ymail.com>
2013-11-17 23:52:55 -0200

    uso de botao para iniciar o programa

--- Trabalho2-SO-Android/src/br/ufrj/dcc/activity/MainActivity.java
+++ Trabalho2-SO-Android/src/br/ufrj/dcc/activity/MainActivity.java
@@ -1,16 +1,35 @@
 package br.ufrj.dcc.activity;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import br.ufrj.dcc.activity.R;
+import android.view.View;
+import android.widget.Button;
+import br.ufrj.dcc.controller.Controlador;
 
 public class MainActivity extends Activity {
+	private Controlador controlador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controlador = Controlador.getInstance();
+        
+        Button iniciaProgramaBtn = (Button)findViewById(R.id.button1);
+        
+        iniciaProgramaBtn.setOnClickListener(new View.OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		getControlador().iniciaPrograma();
+        		Log.d("MAIN_ACTIVITY", "depois controlador.iniciaPRogrma");
+        	}
+        });
+    }
+    
+    public Controlador getControlador(){
+    	return this.controlador;
     }
 
 
