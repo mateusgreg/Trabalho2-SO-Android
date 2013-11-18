5e26a464e06a6f9ac526c5b93ee7dd70a8680b07
ramirezjpdf <ramirez.jpdf@ymail.com>
2013-11-17 23:50:04 -0200

    usa circular queue no lugar da circularArrayList tb da um loop qd n pode mais produzir para liberar os consumidores. usa log para debug

--- Trabalho2-SO-Android/src/br/ufrj/dcc/model/Produtor.java
+++ Trabalho2-SO-Android/src/br/ufrj/dcc/model/Produtor.java
@@ -2,12 +2,14 @@ package br.ufrj.dcc.model;
 
 import java.util.concurrent.Semaphore;
 
-import br.ufrj.dcc.util.CircularArrayList;
+import android.util.Log;
+import br.ufrj.dcc.util.CircularQueue;
+import br.ufrj.dcc.util.Constantes;
 
 public class Produtor extends Ator {
 	
-	public Produtor(CircularArrayList<Recurso> buffer, Semaphore bufferCheioSemaforo, Semaphore temRecursoBufferSemaforo, Semaphore mutex){
-		super(buffer, bufferCheioSemaforo, temRecursoBufferSemaforo, mutex);
+	public Produtor(CircularQueue buffer, GerenteRecurso gerenteRecurso, Semaphore bufferCheioSemaforo, Semaphore temRecursoBufferSemaforo, Semaphore mutex){
+		super(buffer, gerenteRecurso, bufferCheioSemaforo, temRecursoBufferSemaforo, mutex);
 	}
 	
 	@Override
@@ -19,6 +21,7 @@ public class Produtor extends Ator {
 			if(super.gerenteRecurso.podeProduzir()){
 				
 				buffer.enqueue(new Recurso(super.gerenteRecurso.getIdRecurso()));
+				Log.d("PRODUTOR" + getId(), "" + super.gerenteRecurso.getIdRecurso());
 				super.gerenteRecurso.atualizaProducao();
 				super.mutex.release();
 				
@@ -26,7 +29,8 @@ public class Produtor extends Ator {
 			}
 			else{
 				super.mutex.release();
-				super.temRecursoBufferSemaforo.release(); //pode ocorrer de a producao ter alcancado o max mas este produtor nao sabia e tem um consumidor preso esperando 
+				for(int i = 0; i < Constantes.N_CONSUMIDORES; i++)
+					super.temRecursoBufferSemaforo.release(); //pode ocorrer de a producao ter alcancado o max mas este produtor nao sabia e tem um consumidor preso esperando 
 				break;
 			}	
 		}
