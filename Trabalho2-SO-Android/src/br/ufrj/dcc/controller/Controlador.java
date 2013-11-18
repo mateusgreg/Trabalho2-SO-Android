cb49ee0968b34012c712a2508b49f162211397e6
ramirezjpdf <ramirez.jpdf@ymail.com>
2013-11-17 23:46:13 -0200

    implementacao do controlador.

--- Trabalho2-SO-Android/src/br/ufrj/dcc/controller/Controlador.java
+++ Trabalho2-SO-Android/src/br/ufrj/dcc/controller/Controlador.java
@@ -1,5 +1,66 @@
 package br.ufrj.dcc.controller;
 
-public class Controlador {
+import java.util.concurrent.Semaphore;
 
+import android.util.Log;
+import br.ufrj.dcc.model.Ator;
+import br.ufrj.dcc.model.Consumidor;
+import br.ufrj.dcc.model.GerenteRecurso;
+import br.ufrj.dcc.model.Produtor;
+import br.ufrj.dcc.util.CircularQueue;
+import br.ufrj.dcc.util.Constantes;
+
+public class Controlador {
+	private CircularQueue buffer;
+	private Semaphore bufferCheioSemaforo;
+	private Semaphore temRecursoBufferSemaforo;
+	private Semaphore mutex;
+	private GerenteRecurso gerenteRecurso;
+	
+	private static Controlador instancia;
+	
+	public static Controlador getInstance(){
+		if(instancia == null) instancia = new Controlador();
+		return instancia;
+	}
+	private Controlador(){
+		this.reiniciaDependencias();
+	}
+	
+	private void reiniciaDependencias(){
+		this.buffer = new CircularQueue(Constantes.CAPACIDADE_BUFFER);
+		this.bufferCheioSemaforo = new Semaphore(Constantes.CAPACIDADE_BUFFER);
+		this.temRecursoBufferSemaforo = new Semaphore(0);
+		this.mutex = new Semaphore(1);
+		this.gerenteRecurso = new GerenteRecurso();
+	}
+	
+	public void iniciaPrograma(){
+		this.reiniciaDependencias();
+		
+		Ator[] atores = new Ator[Constantes.N_PRODUTORES + Constantes.N_CONSUMIDORES];
+		
+		for(int i = 0; i < Constantes.N_PRODUTORES; i++){
+			atores[i] = new Produtor(this.buffer, this.gerenteRecurso, this.bufferCheioSemaforo, this.temRecursoBufferSemaforo, this.mutex);
+		}
+		
+		for(int i = Constantes.N_PRODUTORES; i < Constantes.N_PRODUTORES + Constantes.N_CONSUMIDORES; i++){
+			atores[i] = new Consumidor(this.buffer, this.gerenteRecurso, this.bufferCheioSemaforo, this.temRecursoBufferSemaforo, this.mutex);
+		}
+		
+		for(Ator ator : atores){
+			ator.start();
+		}
+		
+		for(Ator ator : atores){
+			try {
+				ator.join();
+				Log.d("CONTROLADOR", "ATOR" + ator.getId() + " ACABOU");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		Log.d("CONTROLADOR", "Acabou");
+	}
 }
