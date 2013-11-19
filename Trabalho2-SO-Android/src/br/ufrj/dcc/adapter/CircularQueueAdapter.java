47d06d3937b7d35c5ce9ba46ee4a9152835b7ec3
ramirezjpdf <ramirez.jpdf@ymail.com>
2013-11-19 06:55:28 -0200

    adapter que é usado com a GridView para preenche-la com itens

---
+++ Trabalho2-SO-Android/src/br/ufrj/dcc/adapter/CircularQueueAdapter.java
@@ -0,0 +1,38 @@
+package br.ufrj.dcc.adapter;
+
+import br.ufrj.dcc.util.Constantes;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class CircularQueueAdapter extends ArrayAdapter<String>{
+	private Context ctx;
+	
+	public CircularQueueAdapter(Context context, int resource, String[] items) {
+		super(context, resource, items);
+		this.ctx = context;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent){
+		TextView textView = (TextView)convertView;
+		if(convertView == null){
+			convertView = new TextView(this.ctx);
+			textView = (TextView)convertView;
+		}
+		
+		if(this.getItem(position) != null){
+			if(this.getItem(position).toString().equals(Constantes.UICircularQueueRelated.COM_ITEM)){
+				textView.setBackgroundColor(Constantes.UICircularQueueRelated.COR_COM_ITEM);
+			}
+			else{
+				textView.setBackgroundColor(Constantes.UICircularQueueRelated.COR_SEM_ITEM);
+			}
+		}
+		
+		return convertView;
+	}
+
+}
