2c6583770c1b6bc7c1116159922a6200190b88da
ramirezjpdf <ramirez.jpdf@ymail.com>
2013-11-19 10:39:39 -0200

    QTD_RECURSOS = 1000 e capacidade buffer = 50 mudanca no layout

--- Trabalho2-SO-Android/src/br/ufrj/dcc/util/Constantes.java
+++ Trabalho2-SO-Android/src/br/ufrj/dcc/util/Constantes.java
@@ -3,10 +3,10 @@ package br.ufrj.dcc.util;
 import android.graphics.Color;
 
 public class Constantes {
-	public final static int QTD_MAX_RECURSOS = 100;
+	public final static int QTD_MAX_RECURSOS = 1000;
 	public final static int N_PRODUTORES = 2;
 	public final static int N_CONSUMIDORES = 3;
-	public final static int CAPACIDADE_BUFFER = 20;
+	public final static int CAPACIDADE_BUFFER = 50;
 	
 	public static class UICircularQueueRelated{
 		public final static String COM_ITEM = "#FF0000";
